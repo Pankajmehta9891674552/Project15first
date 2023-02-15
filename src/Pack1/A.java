1d0edfb5ab4b99d77e42857ba09a1edb0c3b80b6
PANKAJMEHTA <pankaj.mehta989@gmail.com>
2023-02-15 17:48:08 +0530

    15 feb first comment

---
+++ src/Pack1/A.java
@@ -0,0 +1,15 @@
+package Pack1;
+
+public class A 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("A CALLING FILE");
+	}
+	
+	
+	
+	
+}
---
+++ src/Pack1/B.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+public class B 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("B CALLING FILE");
+	}
+	
+	
+	
+	
+	
+	
+}
